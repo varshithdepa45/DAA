c6223e307711fb66a9d3befc1336fa9cf9231d81
Depa Varshith Reddy <varshithreddy987@gmail.com>
2026-04-21 05:38:32 +0530

    Refactor quicksort implementation and main function

--- quicksort.c
+++ quicksort.c
@@ -1,58 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int *a,n;
-int i,j;
-int partition(int,int);
-void quicksort(int p,int q){
-	if(p<q){
-		j = partition(p,q);
-		quicksort(p,j-1);
-		quicksort(j+1,q);
-	}
-}
-int partition(int m , int p){
-	int v = a[m];
-	i = m;
-	j = p+1;
-	do{
-		do{
-			i++;
-		}while(i<=p && a[i]<=v);
-		do{
-			j--;
-		}while(j>=0 && a[j]>v);
-		if(i<j)
-		{
-			int t = a[i];
-			a[i] = a[j];
-			a[j] = t;
-		}
-	}while(i<=j);
-	a[m] = a[j];
-	a[j] = v;
-	return j;
-}
-void main(){
-	int i,j;
-	double st,et,tt;
-	printf("Enter no of elements");
-	scanf("%d",&n);
-	a = (int *)malloc(n*sizeof(int));
-	for(i=0;i<n;i++)
-                //a[i] = (rand() %(n-0+1)) +0;
-                a[i] = rand()%n;
-		
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-	st = clock();
-	quicksort(0,n-1);
-	et = clock();
-	tt = (double)(et-st)/CLOCKS_PER_SEC;
-        printf("\nTime taken = %.6lf",tt);
-	/*
-	printf("Elements after sort are: ");
-	for(i=0;i<n;i++)
-		printf("%d\t",a[i]);
-	*/
-	
+int a[1000], n;
+
+void swap(int *x, int *y) {
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+int partition(int low, int high) {
+    int pivot = a[high];   // take last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (a[j] <= pivot) {
+            i++;
+            swap(&a[i], &a[j]);
+        }
+    }
+
+    swap(&a[i + 1], &a[high]);
+    return i + 1;
+}
+
+void quicksort(int low, int high) {
+    if (low < high) {
+        int pi = partition(low, high);
+        quicksort(low, pi - 1);
+        quicksort(pi + 1, high);
+    }
+}
+
+int main() {
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    srand(time(0));
+
+    for (int i = 0; i < n; i++) {
+        a[i] = rand() % n;
+    }
+
+    quicksort(0, n - 1);
+
+    printf("\nSorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
 }
